a528c6f18a64d03d22a933b495882dc724bebba3
Boris Kumpar <boris.kumpar@cadcam-group.eu>
2016-06-20 11:40:33 +0200

    increased decimal precision for global discount percent

--- ccg_sales/models/sale_order.py
+++ ccg_sales/models/sale_order.py
@@ -27,8 +27,8 @@ class SaleOrderLine(models.Model):
     _name = 'sale.order.line'
     _inherit = ['sale.order.line'] 
  
-    discount1_percent = fields.Float('Discount', digits=dp.get_precision('Discount Percent'))
-    discount2_percent = fields.Float('Second discount', digits=dp.get_precision('Discount Percent'))
+    discount1_percent = fields.Float('Discount', digits=dp.get_precision('Discount Percent'),digits_compute=dp.get_precision('Discount Percent'))
+    discount2_percent = fields.Float('Second discount', digits=dp.get_precision('Discount Percent'),digits_compute=dp.get_precision('Discount Percent') )
        
     def on_change_line_discount_total(self, cr, user, ids, discount_total, price_unit, global_discount_percent, context=None ):
         discount1_percent=100.00*discount_total/(price_unit*(100.00-global_discount_percent)/100.00)
@@ -37,7 +37,7 @@ class SaleOrderLine(models.Model):
 class SaleOrder(models.Model):
     _name = 'sale.order'
     _inherit = 'sale.order'
-    global_discount_percent = fields.Float('Global discount', digits=dp.get_precision('Discount Percent'), readonly=False)
+    global_discount_percent = fields.Float('Global discount', digits=dp.get_precision('Discount Percent'), digits_compute=dp.get_precision('Discount Percent'), readonly=False)
     
     def on_change_additional_discount_amount(self,cr,user,ids, additional_discount_amount, list_amount,discount_total, global_discount_percent,  context=None  ):
         if global_discount_percent:

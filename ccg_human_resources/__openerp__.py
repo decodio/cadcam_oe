f823a87498b7e0b20382a7c1968ac03ceaf202cd
Boris Kumpar <boris.kumpar@cadcam-group.eu>
2016-06-28 17:23:34 +0200

    new module added: ccg_human_resources, new field 'year' on holidays_status model and form

---
+++ ccg_human_resources/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2016 CADCAM Design Centar d.o.o. (<http://www.cadcam-group.eu/>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    "name" : "CCG human resources module",
+    "version" : "1.0",
+    "author" : "CADCAM Design Centar d.o.o.",
+    "category" : "CCG internals",
+    'description' : "Customization of human resources module, specific to CADCAM group",
+     "depends" : [
+                 "base",
+                 "sp_cadcam",
+                 "hr_holidays",
+                ],
+    'data' : [
+                "reports/human_resources_reports.xml",
+                "views/hr_holidays_status_form.xml",
+              ],
+    'demo' : [],
+    'installable': True,
+}

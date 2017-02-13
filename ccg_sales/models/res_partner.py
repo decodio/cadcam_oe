d78865a6c025e563b4b1456da458d2d60c4c264d
dario <dario.meniss@slobodni-programi.hr>
2017-02-13 18:26:47 +0100

    puca produkcija

--- ccg_sales/models/res_partner.py
+++ ccg_sales/models/res_partner.py
@@ -23,7 +23,7 @@ from openerp import models, fields, api, tools, _
 
 
 class ResPartnerCCG(models.Model):
-    _name = 'res.partner.ccg'
+    # _name = 'res.partner.ccg'
     _inherit = 'res.partner'
     
     priority = fields.Selection([('0','Low'), ('1','Normal'), ('2','High')], 'Priority', default = '0')

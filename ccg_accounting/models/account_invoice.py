dfbf28f561adcc0188dd4fbecf5fa28aaa37d3d3
Boris Kumpar <boris.kumpar@cadcam-group.eu>
2017-10-23 16:16:38 +0200

    added list on advanced invoices on invoice report (advance invoices specification)

--- ccg_accounting/models/account_invoice.py
+++ ccg_accounting/models/account_invoice.py
@@ -31,6 +31,7 @@ class account_invoice(models.Model):
     advance_amount_total = fields.Float('Advance Total', compute='_compute_amount_total', store=False)
     advance_amount_untaxed = fields.Float('Advance Untaxed', compute='_compute_amount_untaxed', store=False)
     advance_amount_tax = fields.Float('Advance Tax', compute='_compute_amount_tax', store=False)
+    advance_invoices = fields.Char('List of advance invoices', compute='_list_of_advance_invoices', store=False)
 
     @api.one
     def _compute_amount_total(self):
@@ -48,7 +49,7 @@ class account_invoice(models.Model):
                 amount += advance_invoice.amount_untaxed
         self.amount_untaxed
         self.advance_amount_untaxed = amount
-    
+
     @api.one
     def _compute_amount_tax(self):
         amount = 0.0
@@ -56,6 +57,14 @@ class account_invoice(models.Model):
             for advance_invoice in self.advance_invoice_ids:
                 amount += advance_invoice.amount_tax
         self.advance_amount_tax = amount
+    
+    @api.one
+    def _list_of_advance_invoices(self):
+         l =[]
+         if self.advance_invoice_ids:
+             for advance_invoice in self.advance_invoice_ids:
+                 l.append(advance_invoice.internal_number)
+         self.advance_invoices = ', '.join(l)
     
     def on_change_date_invoice(self,cr,user,ids, date_invoice, invoicing_datetime, date_delivery,context=None ):
         """
